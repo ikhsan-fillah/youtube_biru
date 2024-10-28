6b9b280afbab8ba1f4fc1d4724ef9cf33b834a87
Ikhsan Fillah Hidayat <ikhsanfillahhidayat@gmail.com>
2024-10-28 21:53:03 +0700

    menambahkan file yang di butuhkan setiap folder

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ $dbname = 'youtube_biru'; // Your database name
 $username = 'root'; // Your database username
 $password = ''; // Your database password
 
-$connect = new mysqli($host,$dbname,$username,$password);
+$conn = new mysqli($host,$dbname,$username,$password);
 if ($connect->connect_error) {
     die('maaf koneksi gagal: '.$connect->connect_error);
 }
