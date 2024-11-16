b5248936d3e6ee89b77eecfc296bdf3b0fc4a748
Ikhsan Fillah Hidayat <ikhsanfillahhidayat@gmail.com>
2024-11-16 16:32:40 +0700

    file halaman utama

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>YouBlue</title>
     <link rel="stylesheet" href="assets/css/output.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
@@ -17,27 +17,18 @@
     </style>
 </head>
 <body>
-    <nav class="bg-ytBlue h-16 py-4 px-6 flex justify-between items-center">
-        <div class="flex items-center ml-10"> <!-- Menggunakan ml-auto untuk mendorong ke kanan -->
-            <a href="index.php">
-                <img src="assets/img/youtube.png" alt="Logo Youtube" class="h-14 mr-6" />
-            </a>
-        </div>
-        <div class="flex items-center">
-            <div class="relative">
-                <input type="text" placeholder="Search" class="font-poppins bg-gray-100 text-gray-800 rounded-full w-96 py-2 px-4 -mr-6 focus:outline-none focus:ring-2 focus:ring-red-500" />
-                <button class="bg-red-500 text-white rounded-full py-2 px-4 -ml-6 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500"><i class="fa-solid fa-magnifying-glass"></i></button>
-                <button class="bg-red-500 text-white rounded-full py-2 px-4 ml-3 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500"><i class="fa-solid fa-microphone"></i></button>
-
+    <?php include 'includes/navbarBeforeLgn.php'; ?>
+    <div class="flex h-screen"> <!-- Side Bar -->
+        <?php include 'includes/sidebar.php'; ?>
+        <!-- Main Content -->
+        <div class="flex-1 p-8 text-center">
+            <div class="bg-blue-300 mx-96 p-8 rounded-lg">
+                <h1 class="text-3xl font-bold mb-4">Welcome to YouBlue!</h1>
+                <p class="text-lg font-bold ">Coba telusuri untuk memulai</p>
+                <p>Mulailah menonton video untuk membantu kami membuat feed berisi</p>
+                <p>video yang Anda sukai.</p>
             </div>
         </div>
-        <div>
-            <div class="flex items-center mr-9">
-                <button type="button" class="text-white focus:outline-none bg-red-500 rounded-full py-2 px-4 mr-4 hover:bg-red-600 focus:ring-2 focus:ring-red-500"><i class="fa-solid fa-video"></i></button>
-                <button type="button" class="text-white focus:outline-none bg-red-500 rounded-full py-2 px-4 mr-4 hover:bg-red-600 focus:ring-2 focus:ring-red-500">Sign In</button>
-                <button type="button" class="text-white focus:outline-none bg-red-500 rounded-full py-2 px-4 hover:bg-red-600 focus:ring-2 focus:ring-red-500">Sign Up</button>
-            </div>
-        </div>
-    </nav>
+    </div>
 </body>
 </html>
