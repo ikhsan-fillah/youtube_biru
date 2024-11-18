5629e4145541d4567b5a703a24e43973f09696bf
Ikhsan Fillah Hidayat <ikhsanfillahhidayat@gmail.com>
2024-11-18 09:34:01 +0700

    memperbaiki style halaman login

--- auth/login.php
+++ auth/login.php
@@ -12,21 +12,13 @@
 <body class="flex items-center justify-center h-screen ">
     <div class="flex flex-col justify-center w-full items-center  h-[100vh]">
         <div class="mx-auto flex w-full flex-col justify-center px-5 pt-0 md:h-[unset] md:max-w-[50%] lg:h-[100vh] min-h-[100vh] lg:max-w-[50%] lg:px-6">
-            <a class="mt-6 w-fit text-zinc-950 dark:text-white" href="../index.php">
-                <div class="flex w-fit items-center lg:pl-0 lg:pt-0 xl:pt-0">
-                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" class="mr-3 h-[13px] w-[8px] text-zinc-950 dark:text-white" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"></path>
-                    </svg>
-                    <p class="ml-0 text-sm text-zinc-950 dark:text-white ">Back to the homepage</p>
-                </div>
-            </a>
             <div class="bg-blue-500 rounded-lg mb-auto p-8 my-auto flex flex-col md:mt-[70px] w-[350px] max-w-[450px] mx-auto md:max-w-[450px] lg:mt-[110px] lg:max-w-[450px]">
-                <p class="text-[32px] font-bold text-zinc-950 dark:text-white">Sign In</p>
-                <p class="mb-2.5 mt-2.5 font-normal text-zinc-950 dark:text-zinc-400">Enter your email and password to sign in!</p>
+                <p class="text-lg text-[32px] font-bold text-zinc-950 dark:text-white">Login</p>
+                <p class="mb-2.5 mt-2.5 font-normal text-zinc-950 dark:text-zinc-400">Masukan email dan password untuk login</p>
                 <div class="mt-3">
                     <form action="loginProcess.php" method="POST" class="pb-2">
                         <input type="hidden" name="provider" value="google">
-                        <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 w-full text-zinc-950 py-6 dark:text-white" type="submit">
+                        <button class="bg-white inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 w-full text-zinc-950 py-6 dark:text-white" type="submit">
                             <span class="mr-2">
                                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" x="0px" y="0px" viewBox="0 0 48 48" enable-background="new 0 0 48 48" class="h-5 w-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                     <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12 c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24 c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
@@ -39,7 +31,7 @@
                         </button>
                     </form>
                 </div>
-                <div class="relative my-4">
+                <div class="relative">
                     <div class="relative flex items-center py-1">
                         <div class="grow border-t border-zinc-200 dark:border-zinc-700"></div>
                         <div class="grow border-t border-zinc-200 dark:border-zinc-700"></div>
@@ -53,13 +45,12 @@
                                 <input class="mr-2.5 mb-2 h-full min-h-[44px] w-full rounded-lg border border-zinc-200 bg-white px-4 py-3 text-sm font-medium text-zinc-950 placeholder:text-zinc-400 focus:outline-0 dark:border-zinc-800 dark:bg-transparent dark:text-white dark:placeholder:text-zinc-400" id="email" placeholder="name@example.com" type="email" autocapitalize="none" autocomplete="email" autocorrect="off" name="email">
                                 <label class="text-zinc-950 mt-2 dark:text-white" for="password">Password</label>
                                 <input id="password" placeholder="Password" type="password" autocomplete="current-password" class="mr-2.5 mb-2 h-full min-h-[44px] w-full rounded-lg border border-zinc-200 bg-white px-4 py-3 text-sm font-medium text-zinc-950 placeholder:text-zinc-400 focus:outline-0 dark:border-zinc-800 dark:bg-transparent dark:text-white dark:placeholder:text-zinc-400" name="password">
+                                <p><a href="#" class="font-medium text-zinc-950 dark:text-white text-sm text-right">Lupa password?</a></p>
                             </div>
-                            <button class="whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 mt-2 flex h-[unset] w-full items-center justify-center rounded-lg px-4 py-4 text-sm font-medium" type="submit">Sign in</button>
+                            <button class="whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-primary-foreground hover:bg-primary/90 mt-2 flex h-[unset] w-full items-center justify-center rounded-lg px-4 py-4 text-sm font-medium" type="submit">Login</button>
                         </div>
                     </form>
-                    <p><a href="#" class="font-medium text-zinc-950 dark:text-white text-sm">Forgot your password?</a></p>
-                    <p><a href="#" class="font-medium text-zinc-950 dark:text-white text-sm">Sign in via magic link</a></p>
-                    <p><a href="signup.php" class="font-medium text-zinc-950 dark:text-white text-sm">Don't have an account? Sign up</a></p>
+                    <p>Belum punya akun? <a href="signup.php" class="font-medium text-zinc-950 dark:text-white text-sm">Daftar sekarang</a></p>
                 </div>
             </div>
         </div>
